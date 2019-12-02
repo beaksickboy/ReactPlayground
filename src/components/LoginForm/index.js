fa473d1e50eb174790fc0dabf6059662c1bbc988
hapv5 <hapv5@fsoft.com.vn>
2019-12-02 17:02:36 +0700

    Update

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import { useTranslation } from "react-i18next";
 
 const LoginForm = props => {
-    // Extract i18n 
-  const { t } = useTranslation('translations');
+  // Extract i18n
+  const { t } = useTranslation("translations");
+
+  const {username, setUserName} = useState({ username: null });
+
+  const {password, setPassword} = useState({ password: null });
+  
   return (
     <form>
       <div className="form-group">
-        <label for="exampleInputEmail1">{t('username')}</label>
+        <label for="exampleInputEmail1">{t("username")}</label>
         <input type="email" className="form-control" id="exampleInputEmail1" />
       </div>
 
